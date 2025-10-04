5ab3ed65c70df952358a2a9a6ce2216e7f519f18
Твоё Имя <@.com>
2025-10-04 13:31:25 +0300

    feat: hero: add Telegram card tape + styles

--- app/api/forum/events/stream/route.js
+++ app/api/forum/events/stream/route.js
@@ -3,6 +3,7 @@
 
 import { Redis } from '@upstash/redis'
 import { bus } from '../../_bus.js'
+export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export const fetchCache = 'force-no-store'
