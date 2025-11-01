b307e45509979f58f95afcb96bb98f8924108a4f
quantuml7ai <quantuml7ai@gmail.com>
2025-11-01 07:08:37 +0300

    Delete components directory

--- components/WalletTON.js
+++
@@ -1,7 +0,0 @@
-'use client';
-import { TonConnectUI } from '@tonconnect/ui';
-import { useEffect } from 'react';
-export default function WalletTON(){
-  useEffect(()=>{ new TonConnectUI({ manifestUrl:'/tonconnect-manifest.json', buttonRootId:'ton-btn' }); },[]);
-  return <div id="ton-btn" />;
-}
