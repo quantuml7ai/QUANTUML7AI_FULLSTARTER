94deffada6e136a2a9bbfb0c0184d83f2bbc85b1
Твоё Имя <@.com>
2026-01-09 09:46:16 +0200

    feat: hero: add Telegram card tape + styles

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,15 +6,9 @@ export default function HeroSection() {
     <section className="hero-wrap">
       <div className="hero-sticky">
 
-        <HeroAvatar videoSrc="/avatar.mp4" poster="/avatar.jpg" />
-      </div>
-
-      {/* Контент поверх */}
-      <div className="hero-content panel">
-        <h1 style={{ marginTop: 0 }}>Космический интеллект для аналитики</h1>
-        <p>Авторизация через криптокошельки и подписки PRO/VIP.</p>
-        <p><a className="btn" href="/subscribe">Перейти к подписке</a></p>
-      </div>
+        <HeroAvatar poster="/avatar.jpg" />
+      </div> 
+ 
     </section>
   );
 }
