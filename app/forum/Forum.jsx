1c0e1ab817ebd4540804552710cb0e5e44171222
Твоё Имя <@.com>
2026-01-24 23:32:11 +0200

    feat: hero: add Telegram card tape + styles

--- app/forum/Forum.jsx
+++ app/forum/Forum.jsx
@@ -6645,7 +6645,7 @@ const NO_THREAD_OPEN_SELECTOR =
           src={src}
           controls
           playsInline
-          preload="none"
+          preload="metadata"
           className="mediaBoxItem"
           style={{
             objectFit: 'contain', 
@@ -6679,6 +6679,7 @@ const NO_THREAD_OPEN_SELECTOR =
   data-yt-id={videoId}
   data-forum-media="youtube"
   loading="lazy"
+  preload="metadata"
   frameBorder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
   allowFullScreen
@@ -6757,7 +6758,8 @@ const NO_THREAD_OPEN_SELECTOR =
         title="TikTok video"
         data-forum-media="tiktok"
         data-src={`https://www.tiktok.com/embed/v2/${videoId}`} 
-        loading="lazy"       
+        loading="lazy"
+        preload="metadata"       
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
